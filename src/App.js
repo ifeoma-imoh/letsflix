eb6b6dd77cf18fde6cce01a6f3c59d1105b5825b
Ifeoma Imoh <imoh_ifeoma@yahoo.co.uk>
2022-01-24 22:54:35 +0400

    added more categories

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
         fetchURL={requests.fetchNetflixOriginals}
       />
       <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comdey Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/components/Row.js
+++ src/components/Row.js
@@ -25,6 +25,7 @@ function Row({ title, fetchURL }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_URL}${movie.poster_path}`}
             alt={movie.name}
